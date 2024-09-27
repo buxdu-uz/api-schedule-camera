23a61b1e09924002e3a2afad3bc3cc4ccbeb91b1
glpat-5_pov_urCAG7DRKNkMs2 <glpat-5_pov_urCAG7DRKNkMs2>
2024-09-27 10:13:11 +0500

    department nofaol remove

--- app/Domain/Departments/Repositories/DepartmentRepository.php
+++ app/Domain/Departments/Repositories/DepartmentRepository.php
@@ -14,6 +14,7 @@ class DepartmentRepository
     public function getAll()
     {
         return Department::query()
+            ->where('name','not like','%(nofaol)%')
             ->where('h_structure_type',4140)    //FAKULTET
             ->get()
             ->sortBy('name');
