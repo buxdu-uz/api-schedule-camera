4abc164550b2b10bda4cd06ed56accdf7c8bb2f6
bekhruzsalimovsobirovich <bekhruzsalimovsobirovich@gmail.com>
2025-01-29 09:52:23 +0500

    group repository updated, eski yildagi guruhlarni olmaydi

--- app/Domain/Groups/Repositories/GroupRepository.php
+++ app/Domain/Groups/Repositories/GroupRepository.php
@@ -8,7 +8,11 @@ class GroupRepository
 {
     public function getAllGroupDepartmentId($department_id)
     {
+        $year = date('Y');
+        $lastYear = substr($year, -2);
         return Group::query()
+            ->whereRaw("RIGHT(name, 2) REGEXP '^[0-9]+$'") // Oxirgi 2 ta qiymat faqat raqam ekanligini tekshiramiz
+            ->whereRaw("? - CAST(RIGHT(name, 2) AS UNSIGNED) = 4", [$lastYear]) // Farq 4 bo‘lsa
             ->where('department_id',$department_id)
             ->get();
     }
