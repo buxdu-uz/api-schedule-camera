2a0e4430b7736ad315c8cad0e9bcd3d2c3645157
bekhruzsalimovsobirovich <bekhruzsalimovsobirovich@gmail.com>
2025-09-19 11:12:29 +0500

    update department repository getAllfakultet

--- app/Domain/Departments/Repositories/DepartmentRepository.php
+++ app/Domain/Departments/Repositories/DepartmentRepository.php
@@ -15,7 +15,7 @@ class DepartmentRepository
     {
         return Department::query()
             ->where('name','not like','%(nofaol)%')
-            ->where('h_structure_type',4164)    //FAKULTET
+            ->where('h_structure_type',4349)    //FAKULTET
             ->get()
             ->sortBy('name');
     }
